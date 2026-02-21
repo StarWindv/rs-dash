8f17db3f2e5862e9ecf8f32b5eb62531aa0ce822
星灿长风v <2452234@st.shou.edu.cn>
2026-02-22 05:33:58 +0800

    .?这个文件应该跟踪进去了啊，怎么还在外面

--- src/modules/builtins/true.rs
+++ src/modules/builtins/true.rs
@@ -10,8 +10,9 @@ impl Builtin for TrueBuiltin {
     fn name(&self) -> &'static str {
         "true"
     }
-    
+
     fn execute(&self, _shell: &mut Shell, _args: &[String]) -> i32 {
         0
     }
 }
+
